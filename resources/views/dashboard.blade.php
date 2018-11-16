0823348bc5ebcd08d009ae798b2655f2fb841f5e
Milad Teimouri <milad72t@yahoo.com>
2018-11-16 18:40:55 +0330

    bootstrap upgreaded to version 4 (from version 3)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,5 @@
 @include('dashboard_top')
 
-
 <div id="map" style="position:relative ; width: 100%; height: 580px"></div>
 
 @include('dashboard_bottom')
-
--- resources/views/dashboard_top.blade.php
+++ resources/views/dashboard_top.blade.php
@@ -34,7 +34,8 @@
         <div class="col-md-3 left_col">
             <div class="left_col scroll-view">
                 <div class="navbar nav_title" style="border: 0;">
-                    <a href="/dashboard" class="site_title"><img src="/images/SepahLogo-min.png"><span style="font-size: 16px">سامانه مشاهده بلادرنگ پروازها</span></a>
+                    <a href="/dashboard" class="site_title"><img src="/images/SepahLogo-min.png" style=" height: 45px; width: 45px" >
+                        <span style="font-size: 12px">سامانه مشاهده پروازها</span></a>
                 </div>
                 <div class="clearfix"></div>
                 <!-- menu profile quick info -->
@@ -52,20 +53,20 @@
                 <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
                     <div class="menu_section">
                         <ul class="nav side-menu">
-                                <li><a href="/dashboard"><i class="fa fa-desktop"></i> داشبورد<span class="fa fa-chevron-left"></span></a></li>
-                                <li><a><i class="fa fa-desktop"></i> مدیریت پروازها <span class="fa fa-chevron-down"></span></a>
+                                <li><a href="/dashboard"><i class="fa fa-globe"></i> نقشه<span class="fa fa-chevron-left"></span></a></li>
+                                <li><a><i class="fa fa-plane"></i> مدیریت پروازها <span class="fa fa-chevron-down"></span></a>
                                     <ul class="nav child_menu">
                                             <li><a href="#">مشاهده پروازها</a></li>
                                             <li><a href="#">اضافه کردن پرواز</a></li>
                                     </ul>
                                 </li>
-                                <li><a><i class="fa fa-desktop"></i> مدیریت فرودگاه ها <span class="fa fa-chevron-down"></span></a>
+                                <li><a><i class="fa fa-location-arrow"></i> مدیریت فرودگاه ها <span class="fa fa-chevron-down"></span></a>
                                     <ul class="nav child_menu">
                                         <li><a href="#">مشاهده فرودگاه ها</a></li>
                                         <li><a href="#">اضافه کردن فرودگاه</a></li>
                                     </ul>
                                 </li>
-                                <li><a><i class="fa fa-desktop"></i> گزارشات <span class="fa fa-chevron-down"></span></a>
+                                <li><a><i class="fa fa-file-text-o"></i> گزارشات <span class="fa fa-chevron-down"></span></a>
                                     <ul class="nav child_menu">
                                         <li><a href="#">مشاهده گزارشات</a></li>
                                     </ul>
